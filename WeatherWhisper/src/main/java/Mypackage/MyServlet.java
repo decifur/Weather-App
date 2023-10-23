53361c8392504e1123c0daf387157b89e9e180ef
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-23 16:53:42 +0530

    updated java file

--- WeatherWhisper/src/main/java/Mypackage/MyServlet.java
+++ WeatherWhisper/src/main/java/Mypackage/MyServlet.java
@@ -9,6 +9,9 @@ import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 /**
  * Servlet implementation class MyServlet
  */
@@ -62,8 +65,42 @@ public class MyServlet extends HttpServlet {
             responseContent.append(scanner.nextLine());
         }
         
-        System.out.println(responseContent);
         scanner.close();
+        // Parse the JSON response to extract temperature, date, and humidity
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
+        
+      //Date & Time
+        long dateTimestamp = jsonObject.get("dt").getAsLong() * 1000;
+        String date = new Date(dateTimestamp).toString();
+        
+        //Temperature
+        double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+        int temperatureCelsius = (int) (temperatureKelvin - 273.15);
+       
+        //Humidity
+        int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+        
+        //Wind Speed
+        double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+        
+        //Weather Condition
+        String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+        
+     // Set the data as request attributes (for sending to the jsp page)
+        request.setAttribute("date", date);
+        request.setAttribute("city", city);
+        request.setAttribute("temperature", temperatureCelsius);
+        request.setAttribute("weatherCondition", weatherCondition); 
+        request.setAttribute("humidity", humidity);    
+        request.setAttribute("windSpeed", windSpeed);
+        request.setAttribute("weatherData", responseContent.toString());
+        
+        connection.disconnect();
+        
+        
+     // Forward the request to the weather.jsp page for rendering
+        request.getRequestDispatcher("index.jsp").forward(request, response);
         
 	}
 
